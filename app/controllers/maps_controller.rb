bcc65e2145c4a04a06f29dec0de6959d202245b1
ItoSbai <lualaa.design2565@gmail.com>
2024-01-30 17:18:44 +0900

    add:maps_controllerにアクション作成

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,3 +1,25 @@
 class MapsController < ApplicationController
-  def index; end
+  def index
+    @maps = Map.all
+  end
+
+  def new
+    @map = Map.new
+  end
+
+  def create
+    @map = Map.new(map_params)
+    
+    if @map.save
+      redirect_to @map, notice: "Map was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def map_params
+    params.require(:map).permit(:lat, :lng, :text)
+  end
 end

6b75dcb12066d744162e0f8d909a18e0a520f8c4
ItoSbai <lualaa.design2565@gmail.com>
2024-02-03 20:04:54 +0900

    add:search_serviceにsearchメソッドを移行

---
+++ app/services/SearchService.rb
@@ -0,0 +1,11 @@
+class SearchService
+  def self.call(keyword:, lat:, lng:)
+    keyword = params[:keyword]
+    youtube_service = YoutubeService.new(api_key: ENV['GOOGLE_MAP_API_KEY'])
+    @videos = youtube_service.search_videos(keyword, lat, lng)
+  rescue Google::Apis::Error => e
+    # APIからのエラーを適切に処理する
+    flash[:alert] = '動画の検索中にエラーが発生しました。'
+    redirect_to root_path
+  end
+end

5d9621efc7002be77f2c856fdc33db7b6f5bc292
ItoSbai <lualaa.design2565@gmail.com>
2024-02-10 11:57:11 +0900

    feat:ルーティングをRails APIに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  root "maps#index"
-  get 'staticpages/top'
-  resources :users
-  resources :maps
-  resources :videos
+  # root "maps#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :maps
+      resources :videos
+    end
+  end
+  # get 'staticpages/top'
+  # resources :users
+  # resources :maps
+  # resources :videos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

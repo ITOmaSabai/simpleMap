fb72cf4601bb1b1c316c19c7bac431b5b59c1d17
ItoSbai <lualaa.design2565@gmail.com>
2024-02-12 11:30:58 +0900

    add:webpack.config.jsにbabelの設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,22 @@ module.exports = {
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .jsと.jsxファイルに適用
+        exclude: /node_modules/, // node_modules内のファイルは除外
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'] // ES6+とReactのコードをトランスパイル
+          }
+        }
+      }
+    ]
+  }, 
+  resolve: {
+    extensions: ['.js', '.jsx'], // .jsと.jsxの拡張子を解析できるようにする
+  }
 }

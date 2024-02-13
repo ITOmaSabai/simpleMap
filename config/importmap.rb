5f8eca95a86574120f735b2eccd77b4b2d6f12f5
ItoSbai <lualaa.design2565@gmail.com>
2024-02-14 00:51:51 +0900

    fix:importmapをコメントアウト

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,8 @@
-# Pin npm packages by running ./bin/importmap
+# # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "maps/index", to: "maps/index.js"
+# pin "application"
+# pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+# pin "@hotwired/stimulus", to: "stimulus.min.js"
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+# pin_all_from "app/javascript/controllers", under: "controllers"
+# pin "maps/index", to: "maps/index.js"

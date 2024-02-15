360940280009b8f78e5faaa080bb12f1427a132f
ItoSbai <lualaa.design2565@gmail.com>
2024-02-15 12:45:51 +0900

    fix:gitの設定を大文字小文字を区別するよう変更

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Index from '../maps/Index';
+import Index from '../maps';
 
 function App() {
   return (
